032c7a105ce742b7d6056b8114ec56467ea95b76
zafiera <30795388+zafiera@users.noreply.github.com>
2017-12-19 11:09:24 +0200

    no message

---
+++ src/app/pet-cloud-welcome/pet-cloud-welcome.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pet-cloud-welcome',
+  templateUrl: './pet-cloud-welcome.component.html',
+  styleUrls: ['./pet-cloud-welcome.component.css']
+})
+export class PetCloudWelcomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
