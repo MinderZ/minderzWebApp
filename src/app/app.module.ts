d0056f6dfec01d6aec41565823ccc46a4fac0f11
Nicholas Smith <lwandilenick@gmail.com>
2017-12-19 10:43:10 +0200

    WIP:index-halfdone

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
+import { BookServiceComponent } from './components/book-service/book-service.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookServiceComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
