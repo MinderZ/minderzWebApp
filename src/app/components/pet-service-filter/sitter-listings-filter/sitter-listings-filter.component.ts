3e2528b8b743c0418116d6c7b01910e7a728e49c
Unknown <malcolmcarelse@gmail.com>
2018-01-18 15:00:01 +0200

    Geolocation and Pet Sitter Cards

---
+++ src/app/components/pet-service-filter/sitter-listings-filter/sitter-listings-filter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sitter-listings-filter',
+  templateUrl: './sitter-listings-filter.component.html',
+  styleUrls: ['./sitter-listings-filter.component.css']
+})
+export class SitterListingsFilterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
