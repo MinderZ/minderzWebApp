1aaae6adfc18d3d71b1dcbaaa615a0cd09cf0a05
Unknown <malcolmcarelse@gmail.com>
2018-01-19 09:25:31 +0200

    Address Field
    Sitter list page is styled. You can type in a city in the location field and a list of cities based on what you searched will display

--- src/app/components/pet-service-filter/sitter-listings-filter/sitter-listings-filter.component.ts
+++ src/app/components/pet-service-filter/sitter-listings-filter/sitter-listings-filter.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ElementRef, ViewChild, NgZone } from '@angular/core';
+import { MapsAPILoader } from '@agm/core';
+import {} from "@types/googlemaps"
 @Component({
   selector: 'app-sitter-listings-filter',
   templateUrl: './sitter-listings-filter.component.html',
@@ -7,9 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SitterListingsFilterComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('address') public addressElementRef:ElementRef;
+  latitude;
+  longitude;
+  
+  constructor(  private mapLoader:MapsAPILoader,
+    private ngZone:NgZone) { }
 
   ngOnInit() {
+    this.mapLoader.load().then(()=>{
+      let autocomplete = new google.maps.places.Autocomplete(
+        this.addressElementRef.nativeElement,{
+          types:["(cities)"],
+          componentRestrictions: {'country': 'za'}
+        }
+      );
+      autocomplete.addListener("place_changed",()=>{
+          this.ngZone.run(()=>{
+    
+            //Gets place result
+        let place:google.maps.places.PlaceResult
+    
+        //Verify Result
+    
+        if(place.geometry === undefined||place.geometry === null){
+    return;
+        }
+    
+        this.latitude=place.geometry.location.lat()
+        this.longitude=place.geometry.location.lng()
+    
+      });
+      });
+    
+    });
   }
 
 }
