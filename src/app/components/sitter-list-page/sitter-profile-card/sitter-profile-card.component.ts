1d88d8822469e3f49eab334ec34c37fdeb9a8adf
Keenan Idas <k9idas@gmail.com>
2018-01-22 08:31:27 +0200

    Changed style of sitter-listings-filter
    Made it look like the one on live site

--- src/app/components/sitter-list-page/sitter-profile-card/sitter-profile-card.component.ts
+++ src/app/components/sitter-list-page/sitter-profile-card/sitter-profile-card.component.ts
@@ -10,7 +10,7 @@ import { SitterProfileObject } from '../../../model/sitterProfileObject.model';
 export class SitterProfileCardComponent implements OnInit {
 @Input() petSitter={name:"Piet",
 surname:"Plaasmuis",
-description:"I like dogs",
+description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consequat nulla eu urna egestas, lobortis tristique metus congue. Suspendisse interdum eros a sapien accumsan, sed consequat nisi varius. Vestibulum posuere libero in imperdiet euismod. Nullam ut arcu ut lorem lobortis tincidunt id non urna. Quisque sed nulla vitae velit mollis ornare. Aliquam non vehicula lorem. Etiam eget risus rhoncus neque rutrum fermentum.",
 address:"Planet Earth",
 profilePicture:"https://media.giphy.com/media/rWlozpRkj7dU4/source.gif",
 jobs:"Pet Boarding",
