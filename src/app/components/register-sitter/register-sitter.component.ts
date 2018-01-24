1706d93747f23c31ca0b1a143e94a4b08aa26263
Keenan Idas <k9idas@gmail.com>
2018-01-24 12:47:49 +0200

    added heading changing

--- src/app/components/register-sitter/register-sitter.component.ts
+++ src/app/components/register-sitter/register-sitter.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class RegisterSitterComponent implements OnInit {
 
   step:number=1;
+  formName:string = 'Location';
 
   constructor() { }
 
@@ -16,9 +17,28 @@ export class RegisterSitterComponent implements OnInit {
 
   back(){
     this.step-=1;
+    this.changeFormName(this.step);
   }
   next(){
     this.step+=1;
+    this.changeFormName(this.step);
+  }
+
+  submit(){
+
+  }
+
+  changeFormName(step:number){
+    switch(step){
+      case 1 : this.formName = 'Location';
+        break;
+      case 2 : this.formName = 'Personal Details';
+        break;
+      case 3 : this.formName = 'Sitter Preferences';
+        break;
+      case 4 : this.formName = 'Sitter Details';
+        break;
+    }
   }
 
 }
