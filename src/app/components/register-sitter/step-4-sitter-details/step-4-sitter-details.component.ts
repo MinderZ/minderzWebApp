97a19418f53cd969133319760ba106936122e181
Dewald <moutondewald@gmail.com>
2018-01-25 09:14:58 +0200

    Added validation

--- src/app/components/register-sitter/step-4-sitter-details/step-4-sitter-details.component.ts
+++ src/app/components/register-sitter/step-4-sitter-details/step-4-sitter-details.component.ts
@@ -30,16 +30,22 @@ export class Step4SitterDetailsComponent implements OnInit {
   creatingReference = false;
   reference: SitterReferenceObject;
   referenceName: string;
+  validReferenceName = true;
   referencePhone: string;
+  validReferencePhone = true;
   references = new Array();
 
-  constructor(private registerService: RegisterSitterService) { }
+  //Valid form
+  valid = false;
+
+  constructor(protected registerService: RegisterSitterService) { }
 
   ngOnInit() {
   }
 
   addReference() {
-    if (this.referenceName !== undefined && this.referencePhone !== undefined) {
+    if (this.validateReference()) {
+
       this.reference = new SitterReferenceObject;
 
       this.reference.referenceName = this.referenceName;
@@ -49,11 +55,48 @@ export class Step4SitterDetailsComponent implements OnInit {
     }
   }
 
+  validateReference() {
+    if (this.referenceName !== undefined && this.referencePhone !== undefined && this.referencePhone.length === 10) {
+      this.validReferenceName = true;
+      this.validReferencePhone = true;
+      return true;
+
+    } else if (this.referenceName === undefined && this.referencePhone === undefined) {
+      this.validReferenceName = false;
+      this.validReferencePhone = false;
+      return false;
+
+    } else if (this.referenceName !== undefined && this.referencePhone === undefined || this.referencePhone.length !== 10) {
+      this.validReferenceName = true;
+      this.validReferencePhone = false;
+      return false;
+    } else {
+      this.validReferenceName = false;
+      this.validReferencePhone = true;
+      return false;
+    }
+  }
+
   update() {
     this.registerService.sitter.children = this.children;
     this.registerService.sitter.doneVolunteer = this.volunteer;
     this.registerService.sitter.ownPets = this.ownPets;
     this.registerService.sitter.hadPets = this.ownedPets;
+  }
+
+  validate() {
+    if (!this.childrenPristine && !this.ownPetsPristine && !this.volunteer) {
+
+      if (!this.ownPets && !this.ownedPetsPristine) {
+        this.valid = true;
+        this.update();
+      } else {
+        this.valid = false;
+      }
+
+    } else {
+      this.valid = false;
+    }
   }
 
 }
