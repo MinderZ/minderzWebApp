32c058682fcc8cbdf527b541f042ca221341d95c
Dewald <moutondewald@gmail.com>
2018-01-25 07:35:08 +0200

    Implemented register service

--- src/app/services/register-sitter.service.ts
+++ src/app/services/register-sitter.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@angular/core';
+import { SitterProfileObject } from '../model/sitterProfileObject.model';
 
 @Injectable()
-export class CacheService {
+export class RegisterSitterService {
+
+    sitter = new SitterProfileObject;
 
     constructor() { }
 
