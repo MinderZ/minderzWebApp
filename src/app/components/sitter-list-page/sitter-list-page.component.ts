4815de148954ad69a18eb2eaba56cbda698e65da
Unknown <malcolmcarelse@gmail.com>
2018-01-30 13:17:25 +0200

    Fixed Undefined Coordinates error

--- src/app/components/sitter-list-page/sitter-list-page.component.ts
+++ src/app/components/sitter-list-page/sitter-list-page.component.ts
@@ -21,10 +21,11 @@ export class SitterListPageComponent implements OnInit {
   // ]
 
   // sitterList:Client[] = this.filter.filteredList;
-
+  
+constructor(private filter:FilterService,private route:Router,private cache:CacheService) { }
   ngOnInit() {
     this.filter.getbyService(this.filter.serviceTerm)
-  constructor(private filter:FilterService,private route:Router,private cache:CacheService) { }
+  
   }
 
   sitterInfo(sitter:Client){
