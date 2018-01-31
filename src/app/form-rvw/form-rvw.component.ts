effa7a67fb0e3bd27e47a308939e1326f9ae2536
Nicholas Smith <lwandilenick@gmail.com>
2018-01-31 09:13:19 +0200

    nav fix

--- src/app/form-rvw/form-rvw.component.ts
+++ src/app/form-rvw/form-rvw.component.ts
@@ -1,33 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { AuthService } from '../services/auth-service.service';
+import { ReviewTestimonialService } from '../services/review-testimonial.service';
+import { RatingServiceService } from '../services/rating-service.service';
+import { print } from 'util';
 
 @Component({
-  selector: 'app-form-rvw',
-  templateUrl: './form-rvw.component.html',
-  styleUrls: ['./form-rvw.component.css']
+ selector: 'app-form-rvw',
+ templateUrl: './form-rvw.component.html',
+ styleUrls: ['./form-rvw.component.css']
 })
 export class FormRvwComponent implements OnInit {
-  myForm: any;
-  rating: number;
-  constructor() { 
-    this.rating =0;
-    }
+ myForm: any;
+ rating: number;
+ reviewForm: FormGroup;
+ constructor(
+   private fb: FormBuilder,
+   private afAuth: AuthService,
+   private reviewService: ReviewTestimonialService,
+   private ratingServ:RatingServiceService) {
+   this.reviewForm = fb.group({
+     'review': [null, Validators.compose([Validators.required, Validators.maxLength(120)])],
+     'star': [null, Validators.required]
+   })
+   console.log('print')
 
-    onSubmit() {
-      if (this.myForm.valid) {  
-        console.log("Form Submitted!");
-        // This resets the form back to it's initial state  
-        this.myForm.reset();
-      }
-    }
+ }
 
-  ngOnInit() {
-   
-  }
-  setRating(rating:number):void{
-    this.rating = rating;
-    console.log(this.rating);
-    return;
-  }
 
+
+
+ ngOnInit() {
+  this.ratingServ.get_AverageRating("Richard")
+ }
+
+ addReview(post) {
+   console.log(post)
+   setTimeout(() => {
+     this.reviewService.create_review(this.afAuth.currentUserUID(),"Richard", post.review, post.star)
+     alert('Review Posted')
+   }, 1500);
+   this.reviewForm.reset()
+ }
 
 }
